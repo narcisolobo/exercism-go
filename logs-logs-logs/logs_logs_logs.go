9131b973ba58cad47895eb00e2ca19d40d4b95eb
narcisolobo <narcisolobo@gmail.com>
2024-04-29 19:48:40 -0700

    logs logs logs

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,36 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, char := range log {
+		if char == '❗' {
+			return "recommendation"
+		} else if char == '🔍' {
+			return "search"
+		} else if char == '☀' {
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := ""
+	for _, char := range log {
+		if char == oldRune {
+			char = newRune
+		}
+		newLog += string(char)
+	}
+	return newLog
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
